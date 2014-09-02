d85a96c6c745b8bdd81fb573874cf825a2fb2d12
rristow <rodrigo@bol2.com.br>
2014-09-02 19:09:10 +0200

    Added ploneformgen plugin

--- src/collective/googleanalytics/tracking/plugins.py
+++ src/collective/googleanalytics/tracking/plugins.py
@@ -7,6 +7,8 @@ from collective.googleanalytics.bbb import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from collective.googleanalytics.interfaces.tracking import IAnalyticsTrackingPlugin
 from collective.googleanalytics.config import FILE_EXTENSION_CHOICES
+from Products.PloneFormGen.interfaces import IPloneFormGenForm
+from Products.PloneFormGen.interfaces import IPloneFormGenThanksPage
 
 class AnalyticsBaseTrackingPlugin(object):
     """
@@ -82,3 +84,25 @@ class AnalyticsPageLoadTimePlugin(AnalyticsBaseTrackingPlugin):
     """
 
     __call__ = ViewPageTemplateFile('pageloadtime.pt')
+
+class PFGAnalyticsPlugin(AnalyticsBaseTrackingPlugin):
+    """
+    A tracking plugin to track form views, submissions and errors.
+    """
+
+    __call__ = ViewPageTemplateFile('ploneformgen.pt')
+
+    def form_status(self):
+        """
+        Returns the status of the form, which can be None (not a form),
+        'form' (viewing the form), 'thank-you' (form succesfully submitted),
+        or 'error' (form has validation errors).
+        """
+
+        if IPloneFormGenForm.providedBy(self.context):
+            if 'form_submit' in self.request.form.keys():
+                return 'error'
+            return 'form'
+        elif IPloneFormGenThanksPage.providedBy(self.context):
+            return 'thank-you'
+        return None
